19f39ef4b6e169f7138256885f29e0d46a11f7cc
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-03-12 01:15:09 -0500

    Node added, local server up

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
+// worked with dash Taniciea & zyri
+
 document.querySelector('button').addEventListener('click',flipCoin)
 
 let result = document.querySelector('h2')
-
+let heads = document.querySelector('.heads')
+let tails = document.querySelector('.tails')
+let headsScore = 0
+let tailsScore = 0
+ 
 function flipCoin(){
+
 let flip = Math.floor(Math.random() * 2)+ 1
 if(flip % 2 === 0 ){
     result.innerText = 'Heads'
+    headsScore++
+    heads.innerText = headsScore
 }
-else{ result.innerText = 'Tails'}
+else{ 
+result.innerText = 'Tails'
+tailsScore++
+tails.innerText = tailsScore
+}
 }
